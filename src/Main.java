2d8eda50cc9a5d87e1847c65462bf0bf3a37414a
Aline Ribeiro <aline.ribeiro@capgemini.com>
2021-09-28 10:57:05 -0300

    Adiciona metodo media

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,23 @@ public class Main {
 
     private static Scanner sc;
 
+
     public static void main(String[] args) {
-        int num1, num2, resposta;
+
+        int num1, num2, num3, num4, resposta;
+        double respostaMedia;
 
         cabecalho();
         num1 = ler_numero("Digite o primeiro número: ");
         num2 = ler_numero("Digite o segundo número: ");
+        num3 = ler_numero("Digite o primeiro número: ");
+        num4 = ler_numero("Digite o segundo número: ");
         
         resposta = somar(num1, num2);
+        respostaMedia = media(num1, num2, num3, num4);
         
         System.out.printf("Resultado soma: %d ", resposta);
+        System.out.printf("Resultado média: %.2f ", respostaMedia);
 
     }
 
@@ -32,6 +39,14 @@ public class Main {
         int resultado = numero1 + numero2;
         return resultado;
     }
+
+    static double media ( int numero1, int numero2, int numero3, int numero4) {
+        double soma =  numero1 + numero2 + numero3 + numero4;
+        double resultado =  soma / 4;
+        return resultado;
+    }
+
+
 
 
 }
