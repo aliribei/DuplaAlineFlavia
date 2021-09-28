3086e930ba9ef46dcc4d7bfde136dfdb6e877565
Flavia Jaconis <fla_njaconis@yahoo.com.br>
2021-09-28 10:14:39 -0300

    Adiciona Métodos Main e Cabeçalho

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        cabecalho();
+    }
     
+    static void cabecalho(){
+        System.out.println("\n================== CÁLCULO DE MÉDIA ================== ");
+    }
 }
+
